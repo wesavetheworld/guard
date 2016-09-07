65328f78696eeb32f7d6305b242dfc85e1e550e6
Nemanja Avramovic <avramyu@gmail.com>
2016-09-07 23:33:15 +0200

    added table and question helpers

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -3,8 +3,10 @@
 use Avram\Guard\Exceptions\GuardFileException;
 use Avram\Guard\Services\GuardFile;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 abstract class BaseCommand extends Command
 {
@@ -38,5 +40,21 @@ abstract class BaseCommand extends Command
         $errorMessages  = array($title, $message);
         $formattedBlock = $formatter->formatBlock($errorMessages, 'error', true);
         $this->outputInterface->writeln($formattedBlock);
+    }
+
+    public function table(array $headers, array $rows)
+    {
+        $table = new Table($this->outputInterface);
+        $table
+            ->setHeaders($headers)
+            ->setRows($rows);
+        $table->render();
+    }
+
+    public function confirm($question)
+    {
+        $helper   = $this->getHelper('question');
+        $question = new ConfirmationQuestion($question." [y/N]\n", false);
+        return $helper->ask($this->inputInterface, $this->outputInterface, $question);
     }
 }
